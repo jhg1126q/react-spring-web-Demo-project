c6110ff7dad5c44fe1d8c2c8dd5933fdeafced25
jhg1126q <98995858+jhg1126q@users.noreply.github.com>
2023-12-14 17:36:08 +0900

    🎨라우팅 기능 추가
    task1 라우팅 children 추가

--- board-game-cafe-front/src/pages/Login/Login.jsx
+++ board-game-cafe-front/src/pages/Login/Login.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "../../components/UI/Input/Input";
+import classes from "./Login.module.css";
 
 const Login = () => {
-  const text = "테스트";
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onIdChangeHandler = (event) => {
+    console.log("id test :::: ");
+    setId(event.target.value);
+  };
+
+  const onPasswordChangeHandler = (event) => {
+    console.log("pass test :::: ");
+    setPassword(event.target.value);
+  };
 
   return (
     <>
-      <p>로그인 화면 입니다</p>
-      {text}
+      <form className={classes.login}>
+        <ul>
+          <li>
+            <label>ID</label>
+            <Input value={id} type="text" onChange={onIdChangeHandler} />
+          </li>
+          <li>
+            <label>password</label>
+            <Input
+              value={password}
+              type="password"
+              onChange={onPasswordChangeHandler}
+            />
+          </li>
+        </ul>
+      </form>
     </>
   );
 };
---
+++ board-game-cafe-front/src/pages/User/UserDetail.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserDetail = () => {
+  return (
+    <>
+      <h2>User Detail</h2>
+      <p>내용입니다</p>
+    </>
+  );
+};
+
+export default UserDetail;
